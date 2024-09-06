c2688085c295a3d72719f812aa122f7ed60ad050
JUSTINRR88 <126489355+JUSTINRR88@users.noreply.github.com>
2024-09-06 11:17:53 -0600

    Semana 1: Introducción y Diseño
    Objetivos:
    • Comprender los conceptos básicos de HTML y CSS.
    • Diseñar la estructura inicial de la página web.
    Actividades:
    1. Investigación: Estudiar los fundamentos de HTML y CSS utilizando recursos en línea
    como W3Schools.
    2. Estructura HTML:
    o Crear un archivo HTML.
    o Incluir las etiquetas básicas: <!DOCTYPE html>, <html>, <head>, <title>,
    <body>.
    o Crear secciones para el currículum: header, main, footer.
    3. Estilo CSS:
    o Crear un archivo CSS.
    o Vincular el archivo CSS al HTML.
    o Aplicar estilos básicos a las secciones creadas.
    Entregables:
    • Archivo HTML con la estructura básica.
    • Archivo CSS con los estilos básicos aplicados.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let div_root = document.querySelector(".root")
+div_root.innerHTML = `
+    <header class="styleHeader"></header>
+    <footer class="styleFooter"></footer>
+    <main class="styleMain"></main>
+`
