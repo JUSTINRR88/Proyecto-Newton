30279d0cf8a8b6ea90274ab73240de8c4fec4251
JUSTINRR88 <126489355+JUSTINRR88@users.noreply.github.com>
2024-09-09 21:33:37 -0600

    Estructura 1
    actualizacion desde Asus

---
+++ data/dataPerfil.js
@@ -0,0 +1,11 @@
+let perfilData = [
+    {
+        nombre: "Justin Carlos Bercian Muñoz",
+        img: "https://raw.githubusercontent.com/JUSTINRR88/Imagenes-del-Proyecto/main/a1867b96-0117-4f6e-b930-ec3784d19311.jpg",
+        contacto: "3365-1732",
+        correo: "justinbercian1@gmail.com",
+
+    }
+]
+
+export{perfilData}
--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,28 @@
-alert("hola")
+import { perfilData } from "../data/dataPerfil.js";
+let div_footer = document.querySelector(".styleFooter")
+
+div_footer.innerHTML = `
+
+    <div class="seccion1"></div>
+    <div class="seccion2"></div>
+`;
+
+let div_seccion1 = document.querySelector(".seccion1")
+
+function perfil(){
+    perfilData.forEach(element => {
+        let div = document.createElement("div");         
+        div.innerHTML = `
+        <img src="${element.img}" alt="">
+        <div class="nombre">${element.nombre}</div>
+        <div class="contacto">${element.contacto}</div>
+        <div class="correo">${element.correo}</div>
+        `
+        div_seccion1.appendChild(div);
+
+    });
+}
+
+perfil()
+
+
