75453abcaa39a06356e62d0fc4d029081cf68594
JUSTINRR88 <justinbercian1@gmail.com>
2024-09-11 21:43:49 -0600

    atualizacion desde Alienware

--- js/footer.js
+++ js/footer.js
@@ -8,21 +8,22 @@ div_footer.innerHTML = `
 `;
 
 let div_seccion1 = document.querySelector(".seccion1")
-
+//funcion para agregar mis datos personales 
 function perfil(){
     perfilData.forEach(element => {
-        let div = document.createElement("div");         
+        let div = document.createElement("div");     
+        div.classList.add("descripcionContacto")    
         div.innerHTML = `
         <img src="${element.img}" alt="">
-        <div class="nombre">${element.nombre}</div>
-        <div class="contacto">${element.contacto}</div>
-        <div class="correo">${element.correo}</div>
+        <div class="nombre">Contacto</div>
+        <div class="nombre">Nombre: ${element.nombre}</div>
+        <div class="contacto">Numero: ${element.contacto}</div>
+        <div class="correo">Correo: ${element.correo}</div>
         `
         div_seccion1.appendChild(div);
 
     });
 }
-
 perfil()
 
 
